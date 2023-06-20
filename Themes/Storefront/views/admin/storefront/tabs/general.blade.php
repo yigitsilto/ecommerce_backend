207fc87caf153d98ca059b670baf2b22fcdbd585
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2023-06-20 22:15:52 +0300

    bf

--- Themes/Storefront/views/admin/storefront/tabs/general.blade.php
+++ Themes/Storefront/views/admin/storefront/tabs/general.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-8">
-        {{ Form::text('translatable[storefront_welcome_text]', trans('storefront::attributes.storefront_welcome_text'), $errors, $settings) }}
+        {{ Form::wysiwyg('translatable[storefront_welcome_text]', trans('storefront::attributes.storefront_welcome_text'), $errors, $settings) }}
         {{ Form::select('storefront_theme_color', 'Buton Rengi', $errors, trans('storefront::themes'), $settings) }}
 
         <div class="{{ old('storefront_theme_color', array_get($settings, 'storefront_theme_color')) === 'custom_color' ? '' : 'hide' }}"

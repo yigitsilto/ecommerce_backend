4cb07a9711d9dca13745f4c717032aa46ad757e2
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2023-06-20 22:59:52 +0300

    bf

--- Themes/Storefront/views/admin/storefront/tabs/footer.blade.php
+++ Themes/Storefront/views/admin/storefront/tabs/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-8">
         <div class="box-content clearfix">
 {{--        	{{ Form::select('storefront_footer_tags', trans('storefront::attributes.storefront_footer_tags'), $errors, $tags, $settings, ['class' => 'selectize prevent-creation', 'multiple' => true]) }}--}}
-        	{{ Form::text('translatable[storefront_copyright_text]', trans('storefront::attributes.storefront_copyright_text'), $errors, $settings) }}
+        	{{ Form::wysiwyg('translatable[storefront_copyright_text]', trans('storefront::attributes.storefront_copyright_text'), $errors, $settings) }}
         </div>
     </div>
 </div>

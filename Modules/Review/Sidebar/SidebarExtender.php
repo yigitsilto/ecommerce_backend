fbe250bb684f696f0a10b35597c2ebeaa574f771
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2023-06-20 17:24:23 +0300

    bf

--- Modules/Review/Sidebar/SidebarExtender.php
+++ Modules/Review/Sidebar/SidebarExtender.php
@@ -11,16 +11,16 @@ class SidebarExtender extends BaseSidebarExtender
 {
     public function extend(Menu $menu)
     {
-//        $menu->group(trans('admin::sidebar.content'), function (Group $group) {
-//            $group->item(trans('product::sidebar.products'), function (Item $item) {
-//                $item->item(trans('review::sidebar.reviews'), function (Item $item) {
-//                    $item->weight(30);
-//                    $item->route('admin.reviews.index');
-//                    $item->authorize(
-//                        $this->auth->hasAccess('admin.reviews.index')
-//                    );
-//                });
-//            });
-//        });
+        $menu->group(trans('admin::sidebar.content'), function (Group $group) {
+            $group->item(trans('product::sidebar.products'), function (Item $item) {
+                $item->item(trans('review::sidebar.reviews'), function (Item $item) {
+                    $item->weight(30);
+                    $item->route('admin.reviews.index');
+                    $item->authorize(
+                        $this->auth->hasAccess('admin.reviews.index')
+                    );
+                });
+            });
+        });
     }
 }
